2fd087bb74718887d297712f9828c2471e2deec3
redanthrax <redanthraxx@gmail.com>
2023-06-22 15:53:19 -0700

    routing login and warning

--- server/webserver.go
+++ server/webserver.go
@@ -13,14 +13,25 @@ func startWebserver() {
 	//setup root view
 	http.HandleFunc("/", handleView)
 	http.HandleFunc("/login", handleLogin)
-	log.Println("Listening on port 3000.")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Println("Listening on port 9001.")
+	log.Fatal(http.ListenAndServe(":9001", nil))
 }
 
 func handleView(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL.Path)
 	files := []string{
 		"frontend/templates/base.html",
-		"frontend/login.html",
+	}
+
+	switch r.URL.Path {
+	case "/":
+		files = append(files, "frontend/login.html")
+	case "/agents.html":
+		files = append(files,
+			"frontend/templates/dash_base.html",
+			"frontend/agents.html")
+	default:
+		http.NotFound(w, r)
 	}
 
 	tmpl, err := template.ParseFiles(files...)
@@ -33,5 +44,26 @@ func handleView(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 
+	user := r.FormValue("username")
+	pass := r.FormValue("password")
+	if user == "admin" && pass == "admin" {
+		//handle session stuff
+		w.Header().Add("HX-Redirect", "/agents.html")
+	} else {
+		//send denied fragment
+		tmpl, err := template.ParseFiles("frontend/templates/fragments/warning.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		err = tmpl.ExecuteTemplate(w, "warning", "Access Denied")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
 }
